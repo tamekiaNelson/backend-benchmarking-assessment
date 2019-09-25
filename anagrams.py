4868ef39a1bd58f28faa0edd6f2be3d771e9ecc7
Tamekia N <tamekialashay@outlook.com>
2019-09-25 15:57:17 -0400

    complete

--- anagrams.py
+++ anagrams.py
@@ -8,7 +8,7 @@
     for an arbitrary list of strings.
 
 """
-__author__ = "???"
+__author__ = "tamekiaNelson"
 
 import sys
 
@@ -39,20 +39,21 @@ def find_anagrams(words):
         {'dgo': ['dog'], 'act': ['cat', 'act']}
 
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
+    anagrams = {}
+    for word in words:
+        if alphabetize(word) in anagrams:
+            anagrams[alphabetize(word)].append(word)
+        else:
+            anagrams[alphabetize(word)] = [word]
     return anagrams
 
 
 if __name__ == "__main__":
     # run find anagrams of first argument
     if len(sys.argv) < 2:
-        print "Please specify a word file!"
+        print("Please specify a word file!")
         sys.exit(1)
     else:
         with open(sys.argv[1], 'r') as handle:
             words = handle.read().split()
-            print find_anagrams(words)
+            print(find_anagrams(words))
